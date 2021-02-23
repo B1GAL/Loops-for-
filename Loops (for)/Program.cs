6acac64c4e6c7529babb40c261a654a3d0d8a431
Alexander Upton <Alexander99W@ou.edu>
2021-02-23 17:56:45 -0600

    Add project files.

---
+++ Loops (for)/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Loops__for_
+{
+    class Program
+    {
+        static void Main(string[] args) // alex upton
+        {
+            for (int i = 1; i < 101; i++)
+            {
+                Console.WriteLine(i);
+            }
+            for (int i = 3; i < 1001; i += 3)
+            
+            {
+                Console.WriteLine();
+
+            }
+        }
+    }
+}
